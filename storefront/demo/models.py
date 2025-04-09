9ad7d76fbf40f3ce4fd0a5fb2d5eaa8982f5b6f3
Sparsh-05 <144117066+Sparsh-05@users.noreply.github.com>
2025-04-10 01:44:37 +0530

    Delete storefront inside storefront directory

--- storefront/demo/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from database import db
-
-# Create your models here.
-
-person_collection = db['Person']
--- storefront/playground/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from database import db
-
-# Create your models here.
-
-charger_collection = db['delhiChargers']
